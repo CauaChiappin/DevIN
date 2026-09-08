47f2d36c970e488c918bc2395df25a744e6169f9
Your Name <joao.sousa2@scseduca.com.br>
2026-09-08 07:37:33 -0300

    arrumei a duplicação do cadastro empresa

--- php/cadastro_empresa.php
+++ php/cadastro_empresa.php
@@ -77,11 +77,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <link rel="stylesheet" href="../css/cadastrostyle.css">
 </head>
 <body>
-    <div class="main-container">
-        <section class="left-side">
-            <header class="cadastro-header">
-                <a class="brand-logo" href="index.php">Dev<span>IN</span></a>
-            </header>
 
 <div class="main-container">
     <section class="left-side">
